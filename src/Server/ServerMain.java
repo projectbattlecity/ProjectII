597235b4bf627173ed34847f47bbfd7ad80923f4
thaiduongnd846 <thaiduongnd846@gmail.com>
2016-07-17 20:06:35 +0700

    demo RMi

--- src/Server/ServerMain.java
+++ src/Server/ServerMain.java
@@ -8,8 +8,15 @@ package Server;
 import Models.Player;
 import ServeMap.ServerFrame;
 import Models.Tank;
+import RMI.Models.Game;
+import java.net.MalformedURLException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,16 +24,26 @@ import java.net.Socket;
  */
 public class ServerMain {
 
-  
     public static Player serverTank = new Player(0, 0);
-    public static Player clientTank = new Player(0, 100);
+    //public static Player clientTank = new Player(0, 100);
 
     public static void main(String[] args) {
-        new ServerFrame();
-        
+
+        try {
+            Game objGame = new Game();
+            objGame.getFrame().add(objGame.getPanel());
+            LocateRegistry.createRegistry(1099);
+            Naming.rebind("rmi://localhost:1099/game", objGame);
+
+        } catch (RemoteException ex) {
+            Logger.getLogger(ServerMain.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(ServerMain.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
         utils.utils.map.tanks.add(serverTank);
-        utils.utils.map.tanks.add(clientTank);
-        
+        //utils.utils.map.tanks.add(clientTank);
+
 //       
     }
 
